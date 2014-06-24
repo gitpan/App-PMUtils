a38a6dc45ca05ea176c2c7388f98fc85c9f7055f
Steven Haryanto <stevenharyanto@gmail.com>
2014-06-24 02:03:29 +0000

    Import of SHARYANTO/App-PMUtils-0.01 from CPAN.
    gitpan-cpan-distribution: App-PMUtils
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/App-PMUtils-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/App/PMUtils.pm
@@ -0,0 +1,63 @@
+package App::PMUtils;
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $VERSION = '0.01'; # VERSION
+our $DATE = '2014-06-24'; # DATE
+
+1;
+# ABSTRACT: Command line to manipulate Perl module files
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::PMUtils - Command line to manipulate Perl module files
+
+=head1 VERSION
+
+This document describes version 0.01 of App::PMUtils (from Perl distribution App-PMUtils), released on 2014-06-24.
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities:
+
+ pmpath
+ podpath
+ pmless
+ pmedit
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/App-PMUtils>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-App-PMUtils>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=App-PMUtils>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
