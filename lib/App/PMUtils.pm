01fbd6c5e1e457cbd5feff3b70b45e05683626d7
Steven Haryanto <stevenharyanto@gmail.com>
2014-08-16 02:15:51 +0000

    Import of SHARYANTO/App-PMUtils-0.20 from CPAN.
    gitpan-cpan-distribution: App-PMUtils
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         SHARYANTO/App-PMUtils-0.20.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/App/PMUtils.pm
+++ lib/App/PMUtils.pm
@@ -4,8 +4,8 @@ use 5.010001;
 use strict;
 use warnings;
 
-our $VERSION = '0.19'; # VERSION
-our $DATE = '2014-07-29'; # DATE
+our $VERSION = '0.20'; # VERSION
+our $DATE = '2014-08-16'; # DATE
 
 our $_complete_module = sub {
     require Complete::Module;
@@ -72,7 +72,7 @@ App::PMUtils - Command line to manipulate Perl module files
 
 =head1 VERSION
 
-This document describes version 0.19 of App::PMUtils (from Perl distribution App-PMUtils), released on 2014-07-29.
+This document describes version 0.20 of App::PMUtils (from Perl distribution App-PMUtils), released on 2014-08-16.
 
 =head1 SYNOPSIS
 
